d31b426dad7d37085ea88a8c4d2eea1f45c9bea3
Lucas Ramon <lucasramonsa@gmail.com>
2018-04-18 20:58:01 -0300

    Exercício 2

--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,27 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+let anterior = document.querySelector('#anterior');
+let proximo = document.querySelector('#proximo');
+let slide = document.querySelector('#slide');
+let indice = 0;
+
+anterior.addEventListener('click', () => {	
+	if (indice == 0) {		
+		indice = 4;
+	} else {
+		indice = indice - 1;
+	}
+	slide.src = servidorDasImagens + todasAsImagens[indice];
+});
+
+proximo.addEventListener('click', () => {
+	if (indice == 4) {
+		indice = 0;
+	} else {
+		indice = indice + 1;
+	}
+	slide.src = servidorDasImagens + todasAsImagens[indice];
+});
+
