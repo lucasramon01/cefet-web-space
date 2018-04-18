fe33cef3c54614d2c2621fd29cc98e8840942edf
Lucas Ramon <lucasramonsa@gmail.com>
2018-04-17 21:19:18 -0300

    Exercício 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,17 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+let buttonList = document.querySelectorAll('.botao-expandir-retrair');
+
+for (let button of buttonList) {
+	button.addEventListener('click', () => {
+		if(button.parentNode.classList.contains('expandido')) {
+			button.parentNode.classList.remove('expandido');
+			button.innerHTML = '+'
+		} else {
+			button.parentNode.classList.add('expandido');
+			button.innerHTML = '-'
+		}
+	});
+}
+
+
